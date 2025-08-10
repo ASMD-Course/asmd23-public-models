7f85f4448de27c4e4149e13bd024d7f0f3879939
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2025-08-10 22:22:56 +0200

    feat(u07): solve task 1

--- src/main/scala/u07/examples/StochasticChannel.scala
+++ src/main/scala/u07/examples/StochasticChannel.scala
@@ -11,10 +11,10 @@ object StochasticChannel:
 
   def stocChannel: CTMC[State] = CTMC.ofTransitions(
     Transition(IDLE,1.0 --> SEND),
-    Transition(SEND,100000.0 --> SEND),
-    Transition(SEND,200000.0 --> DONE),
-    Transition(SEND,100000.0 --> FAIL),
-    Transition(FAIL,100000.0 --> IDLE),
+    Transition(SEND,100_000.0 --> SEND),
+    Transition(SEND,200_000.0 --> DONE),
+    Transition(SEND,100_000.0 --> FAIL),
+    Transition(FAIL,100_000.0 --> IDLE),
     Transition(DONE,1.0 --> DONE)
   )
 
--- src/main/scala/u07/examples/StochasticChannelSimulation.scala
+++ src/main/scala/u07/examples/StochasticChannelSimulation.scala
@@ -5,9 +5,19 @@ import java.util.Random
 import u07.examples.StochasticChannel.*
 
 @main def mainStochasticChannelSimulation =
-  Time.timed:
-    println:
-      stocChannel.newSimulationTrace(IDLE, new Random)
-        .take(10)
-        .toList
-        .mkString("\n")
+
+  print:
+    stocChannel.timingPercentage(IDLE, DONE, new Random)(30)
+
+
+
+//  println:
+//    stocChannel.meanHittingTime(IDLE, DONE, new Random)(1000)
+//
+//  Time.timed:
+//    println:
+//      stocChannel.newSimulationTrace(IDLE, new Random)
+//        .take(10)
+//        .toList
+//        .mkString("\n")
+
